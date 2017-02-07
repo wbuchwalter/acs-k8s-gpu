b79f7e7ffafc0c5c79779880e2b71c2fa411db80
William Buchwalter <wbuchwalter@gmail.com>
2017-02-07 18:53:41 -0500

    Update setup-nodes.sh

--- setup-nodes.sh
+++ setup-nodes.sh
@@ -2,5 +2,5 @@
 nodes=$(kubectl get node -o name | grep -o 'k8s-agentpool[0-9]-[0-9]*-[0-9]')
 
 for node in $nodes; do
-        ssh -t -oStrictHostKeyChecking=no $node "echo 'Installing driver on $node...' && curl -L -sf https://gist.githubusercontent.com/wbuchwalter/b9912aae79bdd7be5ae761267c6d5a81/raw/8c9dbbacbbcd74415213da15dd587b3356ae93bf/install-nvidia-driver.sh | sudo sh "
+        ssh -t -oStrictHostKeyChecking=no $node "echo 'Installing driver on $node...' && curl -L -sf https://raw.githubusercontent.com/wbuchwalter/acs-k8s-gpu/master/install-nvidia-driver.sh | sudo sh "
 done
