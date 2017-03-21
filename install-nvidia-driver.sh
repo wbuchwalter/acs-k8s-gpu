015a12f1bd9139bea16622a26c18f215ec906096
Sertaç Özercan <sozercan@gmail.com>
2017-03-20 17:02:46 -0700

    Update install-nvidia-driver.sh

--- install-nvidia-driver.sh
+++ install-nvidia-driver.sh
@@ -11,7 +11,7 @@ sh /tools/NVIDIA-Linux-x86_64-375.20.run -a -s
 nvidia-smi
 
 sudo wget https://gist.githubusercontent.com/brianmingus/5497756754bfbcdaac34d39c2b0f0d71/raw/98e84806716d34bf514d73dbc957b35a709d9f73/nvidia_dev.bash -O /etc/init.d/nvidia
-sudo chmod +x nvidia
+sudo chmod +x /etc/init.d/nvidia
 sudo update-rc.d nvidia defaults
 sudo service nvidia start
 
