f82801c23d32da8435fbb5405e17c81ed44aa243
William Buchwalter <wbuchwalter@gmail.com>
2017-04-14 20:38:28 -0400

    specify target for driver install

--- install-nvidia-driver.sh
+++ install-nvidia-driver.sh
@@ -7,7 +7,7 @@ apt-get install -qqy linux-headers-`uname -r`
 wget -P /tools http://us.download.nvidia.com/XFree86/Linux-x86_64/375.20/NVIDIA-Linux-x86_64-375.20.run
 
 chmod +x /tools/NVIDIA-Linux-x86_64-375.20.run
-sh /tools/NVIDIA-Linux-x86_64-375.20.run -a -s
+sh /tools/NVIDIA-Linux-x86_64-375.20.run -a -s --target /tools/nvidia-driver
 nvidia-smi
 
 sudo wget https://gist.githubusercontent.com/brianmingus/5497756754bfbcdaac34d39c2b0f0d71/raw/98e84806716d34bf514d73dbc957b35a709d9f73/nvidia_dev.bash -O /etc/init.d/nvidia
