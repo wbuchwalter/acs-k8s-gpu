4f85a31ed3e34cf11c2ecc804a75b595c1116990
William Buchwalter <wbuchwalter@gmail.com>
2017-06-20 12:54:06 -0700

    Update driver_install.sh

--- driver_install.sh
+++ driver_install.sh
@@ -7,12 +7,10 @@ apt-get install -qqy linux-headers-`uname -r`
 wget -P /tools http://us.download.nvidia.com/XFree86/Linux-x86_64/375.20/NVIDIA-Linux-x86_64-375.20.run
 
 chmod +x /tools/NVIDIA-Linux-x86_64-375.20.run
-sh /tools/NVIDIA-Linux-x86_64-375.20.run -a -s
-nvidia-smi
 
-sudo wget https://gist.githubusercontent.com/brianmingus/5497756754bfbcdaac34d39c2b0f0d71/raw/98e84806716d34bf514d73dbc957b35a709d9f73/nvidia_dev.bash -O /etc/init.d/nvidia
+sudo wget https://raw.githubusercontent.com/wbuchwalter/acs-k8s-gpu/master/nvidia_dev.bash -O /etc/init.d/nvidia
 sudo chmod +x /etc/init.d/nvidia
 sudo update-rc.d nvidia defaults
 sudo service nvidia start
 
-sudo systemctl restart kubelet.service
+
